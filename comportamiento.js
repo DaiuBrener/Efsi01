3ddbeca00501e6056b47cadac57982f81f2d4027
DaiuBrener <102746468+DaiuBrener@users.noreply.github.com>
2022-05-19 12:10:58 -0300

    Add files via upload

--- comportamiento.js
+++ comportamiento.js
@@ -1,10 +1,24 @@
-function validarNota() {
-    input = document.getElementsByTagName("input").value
-
-    if(input >= 0 && input <= 10){
-        alert("god")
+function validarNota(nota, materia) {
+    if(nota > 0 && nota <= 10){
+        document.getElementById(materia).style.color="green"
+        return true;
     }else{
-        alert("error");
+        document.getElementById(materia).style.color="red"
+        return false;
     }
     
+}
+
+
+function showError() {
+    if(input.validity.valueMissing) {
+      // Si el campo está vacío
+      // muestra el mensaje de error siguiente.
+      emailError.textContent = 'Debe introducir un valor de la nota';
+    } else if(email.validity.typeMismatch) {
+      // Si el campo no contiene una dirección de correo electrónico
+      // muestra el mensaje de error siguiente.
+      emailError.textContent = 'El valor introducido debe ser un numero del 1 al 10';
+    }
+  
 }
