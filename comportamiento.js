43a4178abd901cdcbc739d2ba77a1c88c04fbc29
DaiuBrener <102746468+DaiuBrener@users.noreply.github.com>
2022-05-26 12:12:34 -0300

    Add files via upload

--- comportamiento.js
+++ comportamiento.js
@@ -9,16 +9,26 @@ function validarNota(nota, materia) {
     
 }
 
+function hacerPromedio(notamate, notalengua, notaefsi) {
+  let n1, n2,n3;
+  let promedio = 0;
 
-function showError() {
-    if(input.validity.valueMissing) {
-      // Si el campo está vacío
-      // muestra el mensaje de error siguiente.
-      emailError.textContent = 'Debe introducir un valor de la nota';
-    } else if(email.validity.typeMismatch) {
-      // Si el campo no contiene una dirección de correo electrónico
-      // muestra el mensaje de error siguiente.
-      emailError.textContent = 'El valor introducido debe ser un numero del 1 al 10';
-    }
+  n1= document.getElementById(notamate).value;
+  n2= document.getElementById(notalengua).value;
+  n3= document.getElementById(notaefsi).value;
+
+  parseInt(n1, n2, n3)
+  promedio= n1+n2+n3;
+  console.log(promedio)
+
+  document.getElementById("resultado").innerHTML = promedio;
+
+  if(promedio>6){
+    document.getElementById(promedio).style.color="green"
+    return true;
+  }else{
+    document.getElementById(promedio).style.color="red"
+    return false;
   
+  }
 }
